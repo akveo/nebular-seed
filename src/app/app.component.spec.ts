db24866eaea5064d8f1eda42ff08782fcef26be2
Andrei Padolin <andreipadolin@gmail.com>
2021-05-05 12:11:51 +0300

    fix: remove extra tests, resolve comments

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NbActionsModule, NbButtonModule, NbCardModule, NbDialogModule, NbLayoutModule, NbThemeModule } from '@nebular/theme';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        NbThemeModule.forRoot(),
+        NbCardModule,
+        NbButtonModule,
+        NbDialogModule.forRoot(),
+        RouterTestingModule,
+        NbLayoutModule,
+        NbActionsModule
       ],
       declarations: [
         AppComponent
@@ -20,16 +27,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nebular-demo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('nebular-demo');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to nebular-demo!');
-  });
 });
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { NbDialogService } from '@nebular/theme';
+import { NbButtonModule, NbCardModule, NbDialogModule, NbDialogService, NbThemeModule } from '@nebular/theme';
 
 import { HomeComponent } from './home.component';
 
@@ -11,6 +11,12 @@ describe('HomeComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
+      imports: [
+        NbThemeModule.forRoot(),
+        NbCardModule,
+        NbButtonModule,
+        NbDialogModule.forRoot()
+      ],
       declarations: [ HomeComponent ],
       providers: [ {provide: NbDialogService, useClass: MockDialogService} ]
     })
